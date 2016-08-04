a43d2954e3b33ff3d0a4545eeb578809f98ceb8d
Sam Rosenshein <srosenshein1@gmail.com>
2016-08-04 10:49:23 -0400

    Checkpoint 2: COMPLETE

--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -1,11 +1,11 @@
 (function(){
 	function Room($firebaseArray){
-		var ref = new Firebase("https://bloc-chat-ea6f3.firebaseio.com/rooms");
+		var ref = firebase.database().ref().child("rooms");
 		var rooms = $firebaseArray(ref);
 
 		return {
-			all: rooms, //array of all rooms
-			create: function(room){
+			all: rooms, //array of all room objects
+			create: function(room){ //Function to add new room
 				rooms.$add({name: room});
 			}
 		};
