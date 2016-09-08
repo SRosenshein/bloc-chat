f97d8c90873e23600bd5cc92287458069b6b60be
Sam Rosenshein <srosenshein1@gmail.com>
2016-09-08 18:18:11 -0400

    Checkpoint 6: updated

--- app/scripts/directives/sendMessage.js
+++ app/scripts/directives/sendMessage.js
@@ -1,13 +1,16 @@
 (function(){
 	function sendMessage(){
-		if (currentRoom.length != 0){
-			return {
-				templateUrl: 'templates/directives/sendmessage.html',
-				replace: true,
-				controller: 'MessageCtrl',
-				restrict: 'E'
-			};
-		}	
+		return function(scope, element, attrs){
+			console.log(scope.currentRoom);
+			//if (scope.currentRoom.length != 0){
+				return {
+					templateUrl: 'templates/directives/sendmessage.html',
+					replace: true,
+					controller: 'MessageCtrl',
+					restrict: 'E'
+				};
+			//}
+		};	
 	}
 
 	angular
